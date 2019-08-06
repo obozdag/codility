c2a247727bd75639a1eab6ea18e508be3416f6a3
obozdag <obozdag@gmail.com>
2019-08-06 09:15:39 +0300

    Codibility Lesson 6 - Distinct
    Codibility Lesson 6 - Sorting - Distinct

---
+++ count_distinct_values.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Count Distinct Values</title>
+</head>
+<body>
+	<?php
+
+	function solution($arr)
+	{
+		return count(array_count_values($arr));
+	}
+	?>
+	<a href="https://app.codility.com/demo/results/training4ZAU3X-SD6/">Solution</a>
+</body>
+</html>
